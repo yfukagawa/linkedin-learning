4331ed5981d627d2b70221b78cbaa3ffdd7a7450
yfukagawa <100308707+yfukagawa@users.noreply.github.com>
2023-03-02 17:28:11 -0500

    data-structures arrays

---
+++ programming-fundations-data-structures/arrays/src/JsArray.js
@@ -0,0 +1,12 @@
+var dinnerChoices = [["salad", "soup", "cheese"], ["beef", "chicken", "salmon"]];
+
+let appIndex = 0;
+let mainDishIndex = 1;
+
+let firstApp = dinnerChoices[appIndex][0];
+let secondApp = dinnerChoices[appIndex][1];
+let thirdMainDish = dinnerChoices[mainDishIndex][2];
+
+console.log(firstApp);
+console.log(secondApp);
+console.log(thirdMainDish);
